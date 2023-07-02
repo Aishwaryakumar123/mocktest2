1b09c74d60ef5dbc1f4ec8a6d69dcbcffc71ee35
Aishwaryakumar123 <123494214+Aishwaryakumar123@users.noreply.github.com>
2023-07-02 19:15:30 +0530

    Add files via upload

---
+++ initialize_different_Variables.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class initialize_different_Variables {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int height= 25;
+         double amount= 6.75;
+         String name="Aishwarya";
+        System.out.println(height+" "+amount+" "+name);
+
+    }
+}
